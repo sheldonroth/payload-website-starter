004556f57e7955da85b60e12b16dcb5efda2c511
sheldonroth <junkforsheldon@gmail.com>
2025-12-16 12:51:49 -0700

    Fix PostHero: use name instead of title for categories

--- src/heros/PostHero/index.tsx
+++ src/heros/PostHero/index.tsx
@@ -21,9 +21,9 @@ export const PostHero: React.FC<{
           <div className="uppercase text-sm mb-6">
             {categories?.map((category, index) => {
               if (typeof category === 'object' && category !== null) {
-                const { title: categoryTitle } = category
+                const { name: categoryName } = category
 
-                const titleToUse = categoryTitle || 'Untitled category'
+                const titleToUse = categoryName || 'Untitled category'
 
                 const isLast = index === categories.length - 1
 
