edb1f083df944cb01e36ff441e77190354960d8b
sheldonroth <junkforsheldon@gmail.com>
2025-12-20 12:38:18 -0700

    fix: Force dynamic home page to avoid build-time DB issues

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+
 // Redirect home page to admin since this CMS doesn't serve a frontend
 export default function HomePage() {
     redirect('/admin')
