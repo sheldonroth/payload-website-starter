f393e083c4ca06c2621dcae524e6d8be57dbc990
Sheldon Roth <junkforsheldon@gmail.com>
2025-12-26 14:51:28 -0700

    Add migration for pendingCategoryName field

---
+++ src/migrations/20251226_215114.ts
@@ -0,0 +1,13 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "categories" ALTER COLUMN "slug" DROP NOT NULL;
+  ALTER TABLE "products" ADD COLUMN "pending_category_name" varchar;`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "categories" ALTER COLUMN "slug" SET NOT NULL;
+  ALTER TABLE "products" DROP COLUMN "pending_category_name";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -5,6 +5,7 @@ import * as migration_20251217_025119 from './20251217_025119';
 import * as migration_20251222_182129_add_is_admin from './20251222_182129_add_is_admin';
 import * as migration_20251226_151328 from './20251226_151328';
 import * as migration_20251226_185357 from './20251226_185357';
+import * as migration_20251226_215114 from './20251226_215114';
 
 export const migrations = [
   {
@@ -40,6 +41,11 @@ export const migrations = [
   {
     up: migration_20251226_185357.up,
     down: migration_20251226_185357.down,
-    name: '20251226_185357'
+    name: '20251226_185357',
+  },
+  {
+    up: migration_20251226_215114.up,
+    down: migration_20251226_215114.down,
+    name: '20251226_215114'
   },
 ];
